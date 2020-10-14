4632948ff7248ce765b3c3a8ef318d8a04bbc559
Eli Cusic <tntitans31@gmail.com>
2020-10-14 17:42:50 -0500

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -12,6 +12,11 @@ class StudentsController < ApplicationController
   end
 
   def create
+    params.permit!
+    # binding.pry
+    @student = Student.create(first_name: params[:first_name], last_name: params[:last_name])
+    # @student = Student.create(params[:student])
+    redirect_to student_path(@student)
   end
 
 end
